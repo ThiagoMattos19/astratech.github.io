62453289f370cd5ebe7e8f512e885c29a339a704
Thiago Mattos Da Silva <125784920+ThiagoMattos19@users.noreply.github.com>
2025-08-31 19:30:37 -0300

    Update menu-toggle.js

--- menu-toggle.js
+++ menu-toggle.js
@@ -3,6 +3,11 @@ const menu = document.querySelector('.menu-icon');
 
 toggle.onclick = () => {
     menu.classList.toggle('active');
-
 }
 
+new Swiper('.swiper', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    }
+});
